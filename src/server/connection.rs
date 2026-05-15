af84f2be468961734ee86fbdf80008cc1a8ca9c2
alina-jelenic <alinamarija.jelenic@gmail.com>
2026-05-15 13:42:57 +0200

    handle in main na vajah

--- src/server/connection.rs
+++ src/server/connection.rs
@@ -1,5 +1,7 @@
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpStream;
+use tokio::io::{BufReader, AsyncBufReadExt, AsyncWriteExt};
+
 use crate::server::state::ServerState;
 
 pub struct Connection {
@@ -14,7 +16,42 @@ impl Connection {
     }
 
     // skrbi za klienta
-    pub async fn handle(mut self) {
+    pub async fn handle(self) {
         // kasneje: branje sporočil, dodajanje v sobe, broadcast ...
+        // zaklene stanje da drugi ne morajo dostopat, shranim si kam bom pisal pa od kje bom bral
+        let (tx, mut rx) = {
+            let s = self.state.lock().unwrap();
+            (s.tx.clone(), s.tx.subscribe())
+        };
+
+        let (reader, mut writer) = self.stream.into_split();
+        let mut lines = BufReader::new(reader).lines();
+
+        let user = self.username;
+
+        let _ = tx.send(format!("*** {user} se je pridružil *** "));
+        
+        loop{
+            tokio::select! {
+                line = lines.next_line() => {
+                    match line {
+                        Ok(Some(l)) => {
+                            let _ = tx.send(format!("{user}: {l}"));
+                        }
+                        _ => break,
+                        
+                    }
+                }
+                msg = rx.recv() => {
+                    if let Ok(m) = msg {
+                        if writer.write_all(format!("{m}\n").as_bytes()).await.is_err() {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        let _ = tx.send(format!("*** {user} je odsel ***"));
     }
 }
